09e95552464ace1727938193cdabf43cf9ebc63e
RobertPTC <rptc3000@gmail.com>
2016-09-24 13:49:29 -0400

    port env undefined heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express'),
     path = require('path'),
     open = require('open'),
     app = express();
+console.log('process env ', process.env.PORT);
 app.use(express.static(__dirname));
 app.listen(port, function() {
   console.log('app listening on port ' + port);
